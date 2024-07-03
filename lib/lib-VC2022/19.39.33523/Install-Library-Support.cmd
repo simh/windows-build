7777f5595e5cf9601c13d0afe8bac9a5ee1ef65b
Mark Pizzolato <mark@infocomm.com>
2024-07-02 16:52:14 -1000

    Fix generalized and multiple support for VS2022 versions
    - Update build of libpng16.lib and zlib.lib to VS2022 v17.10.3 for release build

--- lib/lib-VC2022/19.39.33523/Install-Library-Support.cmd
+++ lib/lib-VC2022/19.39.33523/Install-Library-Support.cmd
@@ -5,11 +5,12 @@
 :       Visual Studio 2008, so those are referenced directly with 
 :       specific exceptions handled here
 :
-set _Path=%~dp0..\
+set _VerPath=%~dp0
+set _Path=%_VerPath%..\
 if exist "%_Path%..\Debug" rmdir /s /q "%_Path%..\Debug"
 if exist "%_Path%..\lib-VC2008\Debug"   xcopy /S /I "%_Path%..\lib-VC2008\Debug\*"   "%_Path%..\Debug\"       > NUL 2>&1
-xcopy /S /Y /I "%_Path%Debug\*"   "%_Path%..\Debug\"       > NUL 2>&1
+xcopy /S /Y /I "%_VerPath%Debug\*"   "%_Path%..\Debug\"       > NUL 2>&1
 if exist "%_Path%..\Release" rmdir /s /q "%_Path%..\Release"
 if exist "%_Path%..\lib-VC2008\Release" xcopy /S /I "%_Path%..\lib-VC2008\Release\*" "%_Path%..\Release\"     > NUL 2>&1
-xcopy /S /Y /I "%_Path%Release\*" "%_Path%..\Release\"     > NUL 2>&1
-if exist "%_Path%VisualCVersionSupport.txt" copy /y "%_Path%VisualCVersionSupport.txt" "%_Path%..\"        > NUL 2>&1
+xcopy /S /Y /I "%_VerPath%Release\*" "%_Path%..\Release\"     > NUL 2>&1
+if exist "%_VerPath%VisualCVersionSupport.txt" copy /y "%_VerPath%VisualCVersionSupport.txt" "%_Path%..\"        > NUL 2>&1
