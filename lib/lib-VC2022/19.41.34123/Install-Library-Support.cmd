07cf3a8ca044c1c34309549b4ac2adddc4293b59
Mark Pizzolato <mark@infocomm.com>
2024-10-11 08:59:44 -1000

    Add library support for VS2022 v7.11.5

---
+++ lib/lib-VC2022/19.41.34123/Install-Library-Support.cmd
@@ -0,0 +1,16 @@
+@echo off
+: Tbis procedure installs tbe required object libraries to build using 
+: Visual Studio 2022
+: Note: Most object libraries are completely compatible from 
+:       Visual Studio 2008, so those are referenced directly with 
+:       specific exceptions handled here
+:
+set _VerPath=%~dp0
+set _Path=%_VerPath%..\
+if exist "%_Path%..\Debug" rmdir /s /q "%_Path%..\Debug"
+if exist "%_Path%..\lib-VC2008\Debug"   xcopy /S /I "%_Path%..\lib-VC2008\Debug\*"   "%_Path%..\Debug\"       > NUL 2>&1
+xcopy /S /Y /I "%_VerPath%Debug\*"   "%_Path%..\Debug\"       > NUL 2>&1
+if exist "%_Path%..\Release" rmdir /s /q "%_Path%..\Release"
+if exist "%_Path%..\lib-VC2008\Release" xcopy /S /I "%_Path%..\lib-VC2008\Release\*" "%_Path%..\Release\"     > NUL 2>&1
+xcopy /S /Y /I "%_VerPath%Release\*" "%_Path%..\Release\"     > NUL 2>&1
+if exist "%_VerPath%VisualCVersionSupport.txt" copy /y "%_VerPath%VisualCVersionSupport.txt" "%_Path%..\"        > NUL 2>&1
